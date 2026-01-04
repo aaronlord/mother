89b7527112b65f88649c12f7efc54990fb9f1e3f
Aaron Lord <hire@aaron.codes>
2026-01-04 14:49:09 +0000

    docs: update example

--- example/index.php
+++ example/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// Run this example with:
+// mother --php=8.5 php example/index.php
+
 declare(strict_types=1);
 
 namespace Lord\Mother\Example;
@@ -16,20 +19,68 @@ use Lord\Mother\Mother;
 //     static fn (): \Psr\Container\ContainerInterface => \Illuminate\Container\Container::getInstance()
 // );
 
+// Register custom generators, if needed:
 Mother::register(new IdGenerator());
 
+// Create a PersonData object, overriding the city in the nested AddressData:
 $person = Mother::make(PersonData::class, [
     'address.city' => 'New York',
 ]);
 
+// p.s. Take a look at PersonData to see how names are generated.
+
 dump($person);
 
-$builder = Mother::for(NameValue::class);
+// Results in:
+//
+// ^ Lord\Mother\Example\DataTransferObjects\PersonData^ {#55
+//   +id: Lord\Mother\Example\ValueObjects\PersonIdValue^ {#31
+//     +id: 200740
+//   }
+//   +name: Lord\Mother\Example\ValueObjects\NameValue^ {#25
+//     +forename: "John"
+//     +middlename: null
+//     +surname: "Doe"
+//   }
+//   +gender: Lord\Mother\Example\Enums\GenderEnum^ {#24
+//     +name: "MALE"
+//     +value: "male"
+//   }
+//   +address: Lord\Mother\Example\DataTransferObjects\AddressData^ {#50
+//     +id: Lord\Mother\Example\ValueObjects\AddressIdValue^ {#26
+//       +id: 500641
+//     }
+//     +street: "7ZuNjueEjkt0bWrJNG"
+//     +city: "New York"
+//     +postcode: null
+//     +country: "gnu055dtYTHhUNtOi"
+//   }
+//   +age: -741559
+//   +allowance: 540260.0
+//   +dateOfBirth: DateTimeImmutable @646525131 {#29
+//     date: 1990-06-27 22:18:51.0 UTC (+00:00)
+//   }
+// }
 
-$names = $builder->populateNulls();
-
-$b2 = Mother::for(PersonData::class);
-
-$names = $builder->make(2);
+// Use the builder to create multiple NameValue objects, with more control:
+$names = Mother::for(NameValue::class)
+    ->with('surname', 'Smith')
+    ->populateNulls()
+    ->make(2);
 
 dump($names);
+
+// Results in:
+//
+// ^ array:2 [
+//   0 => Lord\Mother\Example\ValueObjects\NameValue^ {#63
+//     +forename: "jwOXucsBqDVfb"
+//     +middlename: "0aYFAJdoclY4T"
+//     +surname: "Smith"
+//   }
+//   1 => Lord\Mother\Example\ValueObjects\NameValue^ {#62
+//     +forename: "H2HYoV5okM"
+//     +middlename: "bCOUM10ivlGZTjsym"
+//     +surname: "Smith"
+//   }
+// ]
